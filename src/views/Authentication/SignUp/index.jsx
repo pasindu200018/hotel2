fb1c9b0e38fda60e58b1b77cafc8ee3b5833a099
nipun-hub <82352923+nipun-hub@users.noreply.github.com>
2024-06-27 15:52:53 +0530

    Revert "bug update"
    This reverts commit 36e5e3ad0482bb0255d6e8eb54d4a2d6708d616e.

--- src/views/Authentication/SignUp/index.jsx
+++ src/views/Authentication/SignUp/index.jsx
@@ -25,7 +25,7 @@ const Index = () => {
     })
 
     const [step3, setstep3] = useState({
-        password:"",
+        password:"dd",
     })
 
     const updateStep = (e) => {
@@ -82,6 +82,7 @@ const Index = () => {
                                         />
                                     </Link>
                                 </div>
+                                {step3.password}
                                 {signUpStep == 1 ? <Signup className="smooth" setSignUpStep={setSignUpStep} updateStep={updateStep} /> : null}
                                 {signUpStep == 2 ? <SignUpStep2 className="smooth" setSignUpStep={setSignUpStep} updateStep={updateStep} /> : null}
                                 {signUpStep == 3 ? <SignUpStep3 className="smooth" setSignUpStep={setSignUpStep} updateStep={updateStep} /> : null}
