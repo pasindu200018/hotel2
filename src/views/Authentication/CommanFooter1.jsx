df1f97068b11c5aae303df7844ea9b7e0d34c5ee
nipun-hub <hemal@cypsolabs.com>
2024-06-27 11:27:39 +0530

    footer update

--- src/views/Authentication/CommanFooter1.jsx
+++ src/views/Authentication/CommanFooter1.jsx
@@ -9,9 +9,9 @@ const CommanFooter1 = () => {
                 <Row>
                     <Col xl={8} className="text-center">
                         <p className="footer-text pb-0">
-                            <span className="copy-text">Jampack © {new Date().getFullYear()} All rights reserved.</span>
+                            <span className="copy-text">liveinhotels.com © {new Date().getFullYear()} All rights reserved.</span>
                             <Link to="#">Privacy Policy</Link>
-                            <span className="footer-link-sep">|</span><Link to="#">T&amp;C</Link><span className="footer-link-sep">|</span><Link to="#">System Status</Link>
+                            <span className="footer-link-sep">|</span><Link to="#">6C</Link><span className="footer-link-sep">|</span><Link to="#">System Status</Link>
                         </p>
                     </Col>
                 </Row>
