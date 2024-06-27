ecb3e23f08b7e53d41eb1b017aa8e9175e2f5184
pasindu200018 <167278835+pasindu200018@users.noreply.github.com>
2024-06-27 15:47:32 +0530

    Email validation Error

--- src/redux/reducer/api/authSlice.js
+++ src/redux/reducer/api/authSlice.js
@@ -1,23 +1,24 @@
 import { apiSlice } from "./apiSlice";
 
-const USERS_URL = "/auth"
-
+const USERS_URL = "/auth";
 
 export const userApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) =>({
-        register: builder.mutation({
-            query: (data)=>({
-                url:`${USERS_URL}/register`,
-                method: "POST",
-                body: data,
-            })
-        }),
-        validateEmail : builder.mutation({
-            query: (data)=>({
-                url:`${USERS_URL}/validateEmail`,
-                method: "POST",
-                body: data,
-            })
-        })
-    })
-})
+  endpoints: (builder) => ({
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/register`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    validateEmail: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/email-validate`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useRegisterMutation, useValidateEmailMutation } = userApiSlice;
