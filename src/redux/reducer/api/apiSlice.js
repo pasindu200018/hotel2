7062bc8d97f95943cb9f0c282335f3aaa23ddaf4
Dulanjana Lakshan <90233572+DulanjanaLakshan@users.noreply.github.com>
2024-06-29 02:13:10 +0530

    Update apiSlice.js

--- src/redux/reducer/api/apiSlice.js
+++ src/redux/reducer/api/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const BASE_URL = "http://3.93.228.84/api";
+const BASE_URL = "https://16.170.245.118/api";
 
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
 
